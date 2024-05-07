c23c9e8de089a731f4e741dadcd3e94498f69cd6
Fariz313 <fariz313.akbar@gmail.com>
2024-05-07 14:41:56 +0700

    Updates

---
+++ assets/rm3IY3aB.js
@@ -0,0 +1,1 @@
+import{_ as e,x as t,v as o,y as n}from"./Bt_GKZl2.js";const s={},c=n("h1",null,"Content",-1),a=[c];function r(_,d){return o(),t("div",null,a)}const f=e(s,[["render",r]]);export{f as default};

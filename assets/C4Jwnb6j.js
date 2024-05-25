12c98f89a552cdd07c00ea5a78de350a2761c8b6
Fariz313 <fariz313.akbar@gmail.com>
2024-05-25 23:47:45 +0700

    Updates

--- assets/C4Jwnb6j.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,x as t,v as o,y as n}from"./BUozNKlb.js";const s={},c=n("h1",null,"Content",-1),a=[c];function r(_,d){return o(),t("div",null,a)}const f=e(s,[["render",r]]);export{f as default};
